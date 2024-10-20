d00c08099ef30bfca91673268fb872f7ba34c271
requ3zt <fabianolopez33@tutamail.com>
2024-10-20 15:17:09 +0200

    port_scanner.py

--- port_scanner.py
+++ port_scanner.py
@@ -17,8 +17,8 @@ def create_socket():
     s.settimeout(1)
     return s
 
-def port_scanner(port, host, s):
 
+def port_scanner(port, host, s):
     try:
         s.connect((host, port))
         print(colored(f"\n[+] El puerto {port} esta abierto\n", 'green'))
@@ -27,15 +27,14 @@ def port_scanner(port, host, s):
     except(socket.timeout, ConnectionRefusedError):
         s.close()
 
-def scan_ports(ports, target):
 
+def scan_ports(ports, target):
     for port in ports:
         s = create_socket()
         port_scanner(port, target, s)
 
 
 def parse_ports(ports_str):
-
     if '-' in ports_str:
         start, end = map(int, ports_str.split('-'))
         return range(start, end+1)
@@ -45,13 +44,10 @@ def parse_ports(ports_str):
         return (int(ports_str),)
 
 
-
 def main():
-    
     target, ports_str = get_arguments()
     ports = parse_ports(ports_str)
     scan_ports(ports, target)
-
 
 
 if __name__ == '__main__':
